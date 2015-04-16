95cd13e85b4cfe7552479b4fad9b911b3ddf52bc
Tsepo Ntsaba <therealmrntsaba@gmail.com>
2015-04-16 10:08:50 -0200

    Update tester.js
    Updated the unit tests... still doesn't work though.

--- test/tester.js
+++ test/tester.js
@@ -1,64 +1,65 @@
 var buzz = require( "../buzzSpace" );
 
+
 var Create =
-[
-    { param: { user_id: "u00000001", module_id: "COS 301" }, success: false },
-    { param: { user_id: "u00000001", module_id: "COS 555" }, success: true },
-    { param: { user_id: "u15494897", module_id: "COS 666" }, success: false },
-    { param: { user_id: "u00000001", module_id: "COS 666" }, success: true },
-    { param: { user_id: "u00000006", module_id: "COS 332" }, success: false },
-    { param: { user_id: "u00000006", module_id: "COS 000" }, success: true }
+    [
+        { param: { user_id: "u00000001", module_id: "COS301" }, success: false },
+        { param: { user_id: "u00000001", module_id: "COS555" }, success: true },
+        { param: { user_id: "u15494897", module_id: "COS666" }, success: false },
+        { param: { user_id: "u00000001", module_id: "COS666" }, success: true },
+        { param: { user_id: "u00000006", module_id: "COS332" }, success: false },
+        { param: { user_id: "u00000006", module_id: "COS000" }, success: true }
 
 
-];
+    ];
 
 var Close =
-[
-    { param: { user_id: "u00000001", module_id: "COS 301" }, success: true },
-    { param: { user_id: "u00000001", module_id: "COS 301" }, success: false },
-    { param: { user_id: "u00000001", module_id: "COS 555" }, success: false },
-    { param: { user_id: "u15494897", module_id: "COS 666" }, success: true },
-    { param: { user_id: "u00000001", module_id: "COS 666" }, success: true },
-    { param: { user_id: "u00000006", module_id: "COS 332" }, success: false },
-    { param: { user_id: "u00000006", module_id: "COS 000" }, success: true }
-];
+    [
+        { param: { user_id: "u11111111", module_id: "COS301" }, success: true },
+        { param: { user_id: "u00000001", module_id: "COS301" }, success: false },
+        { param: { user_id: "u00000001", module_id: "COS555" }, success: false },
+        { param: { user_id: "u15494897", module_id: "COS666" }, success: true },
+        { param: { user_id: "u00000001", module_id: "COS666" }, success: true },
+        { param: { user_id: "u99999999", module_id: "COS332" }, success: false },
+        { param: { user_id: "u00000006", module_id: "COS000" }, success: true }
+    ];
 
 var Register =
-[
-    { param: { user_id: "u00000001", module_id: "COS 301" }, success: true },
-    { param: { user_id: "u00000001", module_id: "COS 301" }, success: false },
-    { param: { user_id: "u00000001", module_id: "COS 555" }, success: false },
-    { param: { user_id: "u15494897", module_id: "COS 666" }, success: true },
-    { param: { user_id: "u00000001", module_id: "COS 666" }, success: true },
-    { param: { user_id: "u00000006", module_id: "COS 332" }, success: false },
-    { param: { user_id: "u00000006", module_id: "COS 000" }, success: true }
-];
+    [
+        { param: { user_id: "u00000001", module_id: "COS301" }, success: true },
+        { param: { user_id: "u00000001", module_id: "COS301" }, success: false },
+        { param: { user_id: "u00000001", module_id: "COS555" }, success: false },
+        { param: { user_id: "u15494897", module_id: "COS666" }, success: true },
+        { param: { user_id: "u00000001", module_id: "COS666" }, success: true },
+        { param: { user_id: "u00000006", module_id: "COS332" }, success: false },
+        { param: { user_id: "u00000006", module_id: "COS000" }, success: true }
+    ];
 
 var Get =
-[
-    { param: { user_id: "u44444444" }, success: true },
-    { param: { user_id: "u10075268" }, success: true },
-    { param: { user_id: "u11111111" }, success: false },
-    { param: { user_id: "u22222222" }, success: false },
-    { param: { user_id: "u33333333" }, success: false },
-    { param: { user_id: "u11008602" }, success: true }
+    [
+        { param: { user_id: "u44444444" }, success: true },
+        { param: { user_id: "u10075268" }, success: true },
+        { param: { user_id: "u11111111" }, success: false },
+        { param: { user_id: "u22222222" }, success: false },
+        { param: { user_id: "u33333333" }, success: false },
+        { param: { user_id: "u11008602" }, success: true }
 
-];
+    ];
 
 function testCreateBuzzSpace( test )
 {
-    for( var i = 0; i < Users.length; ++i )
+    for( var i = 0; i < Create.length; ++i )
     {
-        test.equal( buzz.createBuzzSpace( Create[ i ].param ), Create[ i ].success );
+        test.equals( buzz.createBuzzSpace( Create[ i ].param ), Create[ i ].success );
         test.done();
     }
 }
 
 function testCloseBuzzSpace( test )
 {
-    for( var i = 0; i < Users.length; ++i )
+    for( var i = 0; i < Close.length; ++i )
     {
-        test.equal( buzz.closeBuzzSpace( Close[ i ].param ), Close[ i ].success );
+        test.equals( buzz.closeBuzzSpace( Close[ i ].param ), Close[ i ].success );
         test.done();
     }
 }
@@ -66,17 +67,17 @@ function testCloseBuzzSpace( test )
 
 function testRegisterOnBuzzSpace( test )
 {
-    for( var i = 0; i < Users.length; ++i )
+    for( var i = 0; i < Register.length; ++i )
     {
-        test.equal( buzz.registerOnBuzzSpace( Register[ i ].param ), Register[ i ].success );
+        test.equals( buzz.registerOnBuzzSpace( Register[ i ].param ), Register[ i ].success );
         test.done();
     }
 }
 function testGetUserProfile( test )
 {
-    for( var i = 0; i < Users.length; ++i )
+    for( var i = 0; i < Get.length; ++i )
     {
-        test.equal( buzz.getUserProfile( Get[ i ].param ), Get[ i ].success );
+        test.equals( buzz.getUserProfile( Get[ i ].param ), Get[ i ].success );
         test.done();
     }
 }
@@ -86,3 +87,4 @@ module.exports.testCreateBuzzSpace = testCreateBuzzSpace;
 module.exports.testCloseBuzzSpace = testCloseBuzzSpace;
 module.exports.testRegisterOnBuzzSpace = testRegisterOnBuzzSpace;
 module.exports.testGetUserProfile = testGetUserProfile;
+
